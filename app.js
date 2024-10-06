fa13e8c7b02ce3d72f572133ce6b983fd336e389
kiennt1096 <kiennt1096@gmail.com>
2024-10-06 11:57:20 +0900

    add edit and delete function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Mảng để lưu trữ task
 let tasks = [];
+let editIndex = -1;
 
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
@@ -13,9 +14,31 @@ function addTask(taskName) {
 
 function renderTask() {
     taskList.innerHTML = '';
-    tasks.forEach(task => {
+    tasks.forEach((task, index) => {
         const li = document.createElement("li");
         li.textContent = task;
+
+        const buttons = document.createElement("div");
+        buttons.classList.add("buttons");
+
+        const editButton = document.createElement("button");
+        editButton.textContent = "Sửa";
+        editButton.classList.add("edit");
+        editButton.addEventListener("click", () => {
+            editTask(index);
+        });
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Xóa";
+        deleteButton.classList.add("delete");
+        deleteButton.addEventListener("click", () => {
+            deleteTask(index);
+        })
+
+        buttons.appendChild(editButton);
+        buttons.appendChild(deleteButton);
+
+        li.appendChild(buttons);
         taskList.appendChild(li);
     });
 }
@@ -32,6 +55,17 @@ function clearInput() {
     taskInput.value = '';
 }
 
+function editTask(index) {
+    taskInput.value = tasks[index];
+    addTaskBtn.textContent = "Cập nhập công việc";
+    editIndex = index;
+}
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTask();
+}
+
 addTaskBtn.addEventListener("click", () => {
     const taskName = taskInput.value.trim();
 
@@ -41,9 +75,18 @@ addTaskBtn.addEventListener("click", () => {
         showError("Vui lòng nhập Nội dung công việc có độ dài nhỏ hơn 30 ký tự");
     } else {
         clearError();
-        addTask(taskName);
+        
+        if (editIndex === -1) {
+            addTask(taskName);
+        } else {
+            tasks[editIndex] = taskName;
+            editIndex = -1;
+            addTaskBtn.textContent = "Thêm công việc";
+            
+        }
+        
         clearInput();
-
+        renderTask();
     }
 });
 
